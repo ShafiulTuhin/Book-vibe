e2e25d87401b8817656f9e38f6cbc8ee9e1a5c1a
ShafiulTuhin <tuhin6488@gmail.com>
2026-04-05 16:45:29 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h2 className="p-1 font-bold text-center py-20 text-5xl">
+        The website is under maintenance
+      </h2>
+    </>
+  );
+}
+
+export default App;
